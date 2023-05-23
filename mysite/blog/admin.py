9b339f5d7ed3affed4fe994827b9917b62168272
Mike <mikezhao980306@gmail.com>
2023-05-22 17:49:42 -0700

    finished django blog, supports listing posts, details and admin page

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -4,4 +4,10 @@ from .models import Post
 @admin.register(Post)   # decorator
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title','slug','author','published','status']
+    list_filter = ['status','created','published','author']
+    search_fields = ['title','body']
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields =['author']
+    date_hierarchy='published'
+    order=['status','published']
 # Register your models here
